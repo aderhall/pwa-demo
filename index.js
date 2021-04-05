3e812b2c1d7514e8b4acbe90116b3f9e1792e0e3
Adrian Hall <muzioclavecin@gmail.com>
2021-04-05 17:34:02 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+"use strict";
+
+let swChannel = null;
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    // Register the service worker
+    navigator.serviceWorker
+      .register("/service_worker.js")
+      .then(() => {
+        document.getElementById("js").textContent = "[Verified: service worker registered successfully]";
+      })
+      .catch(err => {
+        document.getElementById("js").textContent = "[Error: service worker failed to register]";
+        console.log("Service worker failed to register: ", err);
+      });
+    
+    // Create a message channel to communicate with the service worker
+    swChannel = new MessageChannel();
+    // Send the sw one of the ports
+    navigator.serviceWorker.controller.postMessage(
+      { type: 'INIT_PORT' },
+      [swChannel.port2]
+    );
+    // Watch for messages from the sw
+    swChannel.port1.onmessage = messageEvent => {
+      // This is just an example, the service worker could send it actually useful messages
+      console.log(messageEvent.data.payload);
+    }
+    
+    // Check if there is a connection to the server
+    updateConnectionStatus();
+  });
+}
+
+function updateConnectionStatus() {
+  checkConnection().then(connected => {
+    setConnectionStatus(connected);
+    setTimeout(updateConnectionStatus, 5000);
+  });
+}
+
+window.addEventListener("offline", () => {
+  setConnectionStatus(false);
+});
+window.addEventListener("online", () => {
+  setConnectionStatus(true);
+});
+
+function setConnectionStatus(connected) {
+  document.getElementById("offline-notif").className = connected ? "" : "visible";
+}
+
+async function checkConnection() {
+  try {
+    await fetch(window.location.origin, { method: "HEAD" });
+    return true;
+  } catch {
+    return false;
+  }
+}
---
+++ service_worker.js
@@ -0,0 +1,57 @@
+const staticCacheName = "PWADemo";
+const assets = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/index.js",
+  "/icon.png",
+  "/manifest.json"
+];
+let port;
+function postMessage(message) {
+  if (port) {
+    port.postMessage(message);
+  } else {
+    setTimeout(() => {
+      postMessage(message);
+    }, 1);
+  }
+}
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticCacheName).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", fetchEvent => {
+  // event.respondWith intercepts the requests and prevents browser default behavior
+  fetchEvent.respondWith(
+    // Open the site-specific cache
+    caches.open(staticCacheName).then(cache => {
+      // First, try to get the resource from the network
+      return fetch(fetchEvent.request)
+        .then(res => {
+          //postMessage({payload: fetchEvent.request.url})
+          if (fetchEvent.request.method === "GET") {
+            // If the fetch was successful, cache the newly-fetched resource (it may be a more recent version than the last one)
+            cache.put(fetchEvent.request, res.clone());
+          }
+          return res;
+        })
+        .catch(err => {
+          //postMessage({payload: [err, fetchEvent.request.url]});
+          
+          return cache.match(fetchEvent.request);
+        });
+    })
+  );
+});
+
+self.addEventListener("message", messageEvent => {
+  if (messageEvent.data && messageEvent.data.type == "INIT_PORT") {
+    port = messageEvent.ports[0];
+  }
+})
